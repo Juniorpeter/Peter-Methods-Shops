4e4aa5babab3dc757b60fb651d8b31c634f5f0ac
Juniorpeter <89209452+Juniorpeter@users.noreply.github.com>
2024-07-24 19:24:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example script for handling form submission
+document.getElementById('checkout-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Send payment screenshot to the email. Thank you for your purchase!');
+});
